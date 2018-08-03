ede6668f88a14af6d4ac0a4a54dbaa1353ba1b43
guitao <1244397476@qq.com>
2018-08-03 17:32:08 +0800

    update pizza-app

---
+++ pizza-order/src/components/about/Contact.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="card text-dark bg-light mb-3">
+        <div class="card-header">联系我们</div>
+        <div class="card-body">
+            <h4 class="card-title">联系我们</h4>
+            <p class="card-text">1244397476@qq.com</p>
+            <router-link :to=" { name: 'phoneLink'}">手机号码</router-link>
+            <router-link :to=" { name: 'personnameLink'}">姓名</router-link>
+
+            <router-view></router-view>
+        </div>
+    </div>
+</template>
