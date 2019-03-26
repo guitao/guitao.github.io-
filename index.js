4a0d33606e3a9a683cd0520f011c04d3fbf9b069
guitao <1244397476@qq.com>
2019-03-27 00:43:47 +0800

    change index.html

---
+++ index.js
@@ -0,0 +1,57 @@
+(function() {
+    // 小屏幕下的导航栏折叠效果
+    const toggleBtn = document.getElementById('toggle-btn'),
+        navList = document.getElementById('nav-list');
+
+    toggleBtn.addEventListener('click', function(e) {
+        e.preventDefault()
+        navList.style.display = navList.style.display == 'none' ? 'block' : 'none'
+    }, false)
+
+    // 大屏幕下右侧导航的滚动监听
+
+
+
+    window.onscroll = function() {
+        let scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+        // console.log(scrollTop)
+        const navBox = document.getElementById('nav-box')
+        if (scrollTop >= 620) {
+            navBox.style.position = 'fixed'
+            navBox.style.top = '10%'
+        } else {
+            navBox.style.position = 'absolute'
+            navBox.style.top = '100vh'
+        }
+
+        var navItem = document.getElementsByClassName('nav-item')
+            // 选择触发的nav
+        function setActiveDom(dataName) {
+
+            const navItemArr = Array.prototype.slice.call(navItem)
+                // console.log(Array.isArray(navItemArr))
+            navItemArr.forEach(item => {
+                item.classList.remove('active')
+            })
+            navItemArr.forEach(item => {
+                if (item.getAttribute('data-name') == dataName) {
+                    item.classList.add('active')
+                }
+            })
+        }
+
+
+        let target;
+        if (scrollTop <= 1099) {
+            setActiveDom('basc-info')
+        } else if (scrollTop > 1099 && scrollTop <= 1998) {
+            setActiveDom('skill')
+        } else if (scrollTop > 1998 && scrollTop <= 2100) {
+            setActiveDom('project')
+        } else {
+            setActiveDom('contact')
+        }
+
+
+    }
+}())
