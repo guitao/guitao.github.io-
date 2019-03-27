6fd363584d049ed7b616af2e8f69ce7f5c78d8d6
guitao <1244397476@qq.com>
2019-03-27 14:47:59 +0800

    update

--- index.js
+++ index.js
@@ -4,9 +4,9 @@
         navList = document.getElementById('nav-list');
 
     toggleBtn.addEventListener('click', function(e) {
-        e.preventDefault()
-        navList.style.display = navList.style.display == 'none' ? 'block' : 'none'
-    }, false)
+        e.preventDefault();
+        navList.style.display = navList.style.display == 'none' ? 'block' : 'none';
+    }, true);
 
     // 大屏幕下右侧导航的滚动监听
 
@@ -14,28 +14,28 @@
 
     window.onscroll = function() {
         let scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-        // console.log(scrollTop)
-        const navBox = document.getElementById('nav-box')
+        console.log(scrollTop)
+        const navBox = document.getElementById('nav-box');
         if (scrollTop >= 620) {
-            navBox.style.position = 'fixed'
-            navBox.style.top = '10%'
+            navBox.style.position = 'fixed';
+            navBox.style.top = '10%';
         } else {
-            navBox.style.position = 'absolute'
-            navBox.style.top = '100vh'
+            navBox.style.position = 'absolute';
+            navBox.style.top = '100vh';
         }
 
-        var navItem = document.getElementsByClassName('nav-item')
-            // 选择触发的nav
+        var navItem = document.getElementsByClassName('nav-item');
+        // 选择触发的nav
         function setActiveDom(dataName) {
 
-            const navItemArr = Array.prototype.slice.call(navItem)
-                // console.log(Array.isArray(navItemArr))
+            const navItemArr = Array.prototype.slice.call(navItem);
+            // console.log(Array.isArray(navItemArr))
             navItemArr.forEach(item => {
-                item.classList.remove('active')
+                item.classList.remove('active');
             })
             navItemArr.forEach(item => {
                 if (item.getAttribute('data-name') == dataName) {
-                    item.classList.add('active')
+                    item.classList.add('active');
                 }
             })
         }
@@ -44,12 +44,12 @@
         let target;
         if (scrollTop <= 1099) {
             setActiveDom('basc-info')
-        } else if (scrollTop > 1099 && scrollTop <= 1998) {
-            setActiveDom('skill')
-        } else if (scrollTop > 1998 && scrollTop <= 2100) {
+        } else if (scrollTop > 1099 && scrollTop <= 1900) {
+            setActiveDom('skill');
+        } else if (scrollTop > 1900 && scrollTop <= 2500) {
             setActiveDom('project')
         } else {
-            setActiveDom('contact')
+            setActiveDom('contact');
         }
 
 
